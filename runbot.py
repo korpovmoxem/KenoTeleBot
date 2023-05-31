c129f014604292caed6429e45e8acd28c3b2bae2
Max <m.k4rp0v@yandex.ru>
2023-05-31 15:14:58 +0300

    1

--- runbot.py
+++ runbot.py
@@ -3,3 +3,15 @@ from random import random
 import telebot
 
 
+with open('bot_auth.txt', 'r') as file:
+    bot_token = str(file.read())
+bot = telebot.TeleBot(bot_token)
+
+
+
+@bot.message_handler()
+def get_message(message):
+    print(message)
+
+
+bot.infinity_polling()
