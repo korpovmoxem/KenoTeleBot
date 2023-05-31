25671b792b32a330b18c199429abece5b09cfaa9
Max <m.k4rp0v@yandex.ru>
2023-05-31 15:35:24 +0300

    Update runbot.py

--- runbot.py
+++ runbot.py
@@ -11,11 +11,10 @@ bot = telebot.TeleBot(bot_token)
 
 
 pics = [f for f in listdir('wednesday_pics') if isfile(join('wednesday_pics', f))]
-#-1001532546328
 rand_int = random.randrange(len(pics))
 pic_path = '/root/KenoTeleBot/wednesday_pics/' + pics[rand_int]
-
 with open(pic_path, 'rb') as pic:
     pic_file = pic.read()
-bot.send_photo('-1001914064521', pic_file)
+
+bot.send_photo('-1001532546328', pic_file)
 
